e986e15c248cb96fa7517fc56db3e26e1599309d
deykypinheiro <pinheiromikael96@gmail.com>
2021-08-19 19:01:13 -0300

    teste

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: demikael <pinheiromikael96@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 22:28:26 by demikael          #+#    #+#             */
-/*   Updated: 2021/08/14 22:28:27 by demikael         ###   ########.fr       */
+/*   Updated: 2021/08/19 19:01:03 by demikael         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	size_t	i;
 	size_t	strlen;
 
-	if (!s)
+	if (!s || !*s)
 		return ;
 	strlen = ft_strlen(s);
 	i = 0;
